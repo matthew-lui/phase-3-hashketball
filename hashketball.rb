c227d72d6b195ca95d47dbd3dbaa359b519a56d6
matthew-lui <107003143+matthew-lui@users.noreply.github.com>
2022-11-27 14:06:35 -0500

    commit tree 4f317b76e4a9733ec52238646b004ef030f6cc25

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
-# Write your code below game_hash
+require 'pry'
+
+
 def game_hash
   {
     home: {
@@ -124,6 +126,69 @@ def game_hash
       ]
     }
   }
+
 end
 
-# Write code here
+def all_players
+  # get all players then merge them into one array with the concat method or + method
+  game_hash[:home][:players].concat( game_hash[:away][:players])
+end
+
+
+def num_points_scored (name)
+
+  # a few methods will get this .find method is the same in Javascript and only find results that matches
+  all_players.find { |player| player[:player_name]=== name}[:points]
+
+end
+
+def get_player(name)
+
+  # created the get_player helper method that we can reuse to get other elements in array.  
+  all_players.find do |player| player[:player_name] === name end
+end
+
+def num_points_scored(name)
+  # get player name, then grabs points
+  get_player(name)[:points]
+end
+
+def shoe_size(name)
+  # get player then grabs shoe size
+  get_player(name)[:shoe]
+end
+
+def team_colors(team_name)
+  # checks the teams name if brooklyn nets, return home, then colors, otherwise return away and their colors
+  team_name === "Brooklyn Nets" ? game_hash[:home][:colors] : game_hash[:away][:colors]
+end
+
+def team_names
+
+   # goes into the game hash, looks into names, then team_names 
+  [game_hash[:home][:team_name], game_hash[:away][:team_name]]
+end
+
+def get_team_by_name(name)
+  # helper method gets team by n ame , first grabs home team, if not home team, then returns away team
+  game_hash[:home][:team_name] === name ? game_hash[:home] : game_hash[:away]
+end
+
+def player_numbers(team_name)
+
+  #map method maps each team and returns the player, then returns their player number
+  get_team_by_name(team_name)[:players].map do |player| player[:number] end
+end
+
+def player_stats(name)
+
+  #reuse our get player method and pass in name argument to return all player stats. 
+  get_player(name)
+end
+
+def big_shoe_rebounds
+  # learn max_by and sortby method. It will be very useful. 
+  all_players.max_by do |player| player[:shoe] end[:rebounds]
+end
+
+# binding.pry
